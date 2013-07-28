2abab184d7dd0f56aa09f7bbbdd2c73fa743bcc2
Tum Chaturapruek <tummykung@gmail.com>
2013-07-27 22:45:45 -0700

    add web server

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, render_template, url_for, redirect, session, flash, socket
+from flask import Flask, request, render_template, url_for, redirect, session, flash
 import json
-
 import settings
 
+
+port = 2000
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        redirect_uri = url_for('roomba', _external=True)
-        authorize_url = get_authorize_url(redirect_uri)
-        return redirect(authorize_url)
-
-    # Display intro page with login button that posts to here
     return render_template('index.html')
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for(''))
+@app.route('/control', methods=['GET'])
+def control():
+    return render_template('control.html', port=port)
 
 if __name__ == '__main__':
     app.run(debug=True)
