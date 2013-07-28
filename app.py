5321b010106cdf43dc182c18793ce4f9af7cf93c
Tum Chaturapruek <tummykung@gmail.com>
2013-07-28 03:44:03 -0700

    add more frontend

--- app.py
+++ app.py
@@ -7,14 +7,25 @@ import settings
 
 port = 2000
 app = Flask(__name__)
+app.secret_key = settings.SECRET_KEY
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	if request.method == 'POST':
+		session['access_token'] = request.cookies.get('remember_token')
+		return redirect(url_for('control', _external=True))
+	robots = [
+		{"name": "Roomba1", "status": "green"},
+		{"name": "Roomba2", "status": "red"},
+		{"name": "Roomba3", "status": "red"}]
+	return render_template('index.html', robots=robots)
 
-@app.route('/control', methods=['GET'])
+@app.route('/control', methods=['GET', 'POST'])
 def control():
-    return render_template('control.html', port=port)
+	if request.method == 'POST':
+		session.pop('remember_token', None)
+		return redirect(url_for('index', _external=True))
+	return render_template('control.html', port=port)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
--- settings.py
+++ settings.py
@@ -1,12 +1,15 @@
 from os import environ as env
 
 try:
-	# Any seeting happens here. For example, you can do the following:
+    # Any seeting happens here. For example, you can do the following:
     # OAuth2 client ID and secret
     # CLIENT_ID = env['CLIENT_ID']
     # CLIENT_SECRET = env['CLIENT_SECRET']
+    SECRET_KEY = env['SECRET_KEY']
     pass
 
 except KeyError:
+    print 'You need to set SECRET_KEY in the environment for this app'
+    print 'e.g `SECRET_KEY=XXXX python app.py`'
     exit(1)
 
